e552c98d1464a925637479a17ac89c4e91926ec1
Brooke Adlesperger-Jones <badles@live.com>
2023-06-08 16:37:37 -0500

    added render note list function to index.js file

--- routes/index.js
+++ routes/index.js
@@ -12,3 +12,28 @@ const handleNoteView = (e) => {
   const note = JSON.parse(e.target.parentElement.getAttribute('data-note'));
   window.location.href = `/notes#${note.id}`;
 };
+
+const renderNoteList = (notes) => {
+    noteList.innerHTML = '';
+  
+    if (notes.length === 0) {
+      const emptyMessage = document.createElement('li');
+      emptyMessage.classList.add('list-group-item');
+      emptyMessage.innerText = 'No saved Notes';
+      noteList.append(emptyMessage);
+    } else {
+      notes.forEach((note) => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-group-item');
+        listItem.setAttribute('data-note', JSON.stringify(note));
+        listItem.addEventListener('click', handleNoteView);
+  
+        const titleElement = document.createElement('span');
+        titleElement.classList.add('list-item-title');
+        titleElement.innerText = note.title;
+  
+        listItem.append(titleElement);
+        noteList.append(listItem);
+      });
+    }
+  };
