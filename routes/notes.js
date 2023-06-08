f4f28f89907a695c13eaaa52ce4025ed0fa67491
Brooke Adlesperger-Jones <badles@live.com>
2023-06-08 15:51:34 -0500

    added render active note function to notes.js file

--- routes/notes.js
+++ routes/notes.js
@@ -38,3 +38,19 @@ const deleteNote = (id) => {
     },
   });
 };
+
+const renderActiveNote = () => {
+  hide(saveNoteBtn);
+
+  if (activeNote.id) {
+    noteTitle.setAttribute('readonly', true);
+    noteText.setAttribute('readonly', true);
+    noteTitle.value = activeNote.title;
+    noteText.value = activeNote.text;
+  } else {
+    noteTitle.removeAttribute('readonly');
+    noteText.removeAttribute('readonly');
+    noteTitle.value = '';
+    noteText.value = '';
+  }
+};
